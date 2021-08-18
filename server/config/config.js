7b2ea943921adc6f2d934a94d13b93092a57bedc
Olmeiro Orozco <olme.orozco@gmail.com>
2021-08-17 21:14:57 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB = "mongodb+srv://olme:dfZ5jnbCB6TCyKBD@cluster0.lqg2v.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
